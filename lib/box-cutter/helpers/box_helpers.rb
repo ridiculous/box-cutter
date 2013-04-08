8a05b4ca6f265b6ee313b847393b53da66fca42b
Ryan Buckley <arebuckley@gmail.com>
2013-04-07 15:18:03 -1000

    styling updates

--- lib/box-cutter/helpers/box_helpers.rb
+++ lib/box-cutter/helpers/box_helpers.rb
@@ -16,7 +16,7 @@ module BoxCutter
       options[:span].insert(0, 'span') if options[:span]
       options[:offset].insert(0, 'offset') if options[:offset]
       options[:color].insert(0, 'box-') if options[:color]
-      options[:links] = [options[:links]] if options[:links] && !options[:links][0].is_a?(Array)
+      options[:links] = [options[:links]] if options[:links].any? && !options[:links][0].is_a?(Array)
       options[:klass] = options.delete(:class)
 
       if options.delete(:if)
